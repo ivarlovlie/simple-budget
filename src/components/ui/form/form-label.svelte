f6ac4e6691ce0966299220e3366978dea9f437b8
ivar <i@oiee.no>
2024-08-05 18:05:26 +0200

    Update components

--- src/components/ui/form/form-label.svelte
+++ src/components/ui/form/form-label.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
 import { Label } from "$components/ui/label/index.js";
-import { cn } from "$utils/ui.js";
+import * as m from "$strings";
+import { cn } from "$utils/ui";
 import type { Label as LabelPrimitive } from "bits-ui";
 import { getFormControl } from "formsnap";
 
-type $$Props = LabelPrimitive.Props;
+type $$Props = LabelPrimitive.Props & { required?: boolean };
 
-let className: $$Props["class"] = undefined;
+const className: $$Props["class"] = undefined;
 export { className as class };
+
+export const required: boolean = false;
 
 const { labelAttrs } = getFormControl();
 </script>
 
-<Label {...$labelAttrs} class={cn("data-[fs-error]:text-destructive", className)} {...$$restProps}>
+<Label
+	{...$labelAttrs}
+	class={cn('data-[fs-error]:text-destructive', className)}
+	{...$$restProps}
+	aria-required={required ?? false}
+	title={$$restProps.title ? $$restProps.title + '. ' + m.requiredField() : m.requiredField()}
+>
 	<slot {labelAttrs} />
+	{#if required ?? false}
+		<span class="text-red-500">&ast;</span>
+	{/if}
 </Label>
